82479cd5eb2ef99114946811ac1498a8270e02df
Arif Dogan <me@arif.sh>
2025-02-06 16:26:48 +0100

    Update giq.rb formula: switch to latest release URLs, add license, and improve syntax consistency.

--- formula/giq.rb
+++ formula/giq.rb
@@ -1,32 +1,36 @@
 class Giq < Formula
-  desc "Git wrapper enhanced with AI-powered features"
-  homepage "https://github.com/doganarif/giq"
-  version "0.1.0"
+  desc 'Git wrapper enhanced with AI-powered features'
+  homepage 'https://github.com/doganarif/giq'
+  license 'MIT'
+  head 'https://github.com/doganarif/giq.git', branch: 'main'
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_darwin_arm64.tar.gz"
+      url 'https://github.com/doganarif/giq/releases/latest/download/giq_darwin_arm64.tar.gz'
     else
-      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_darwin_amd64.tar.gz"
+      url 'https://github.com/doganarif/giq/releases/latest/download/giq_darwin_amd64.tar.gz'
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_linux_amd64.tar.gz"
-    end
+    url 'https://github.com/doganarif/giq/releases/latest/download/giq_linux_amd64.tar.gz' if Hardware::CPU.intel?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_linux_arm64.tar.gz"
+      url 'https://github.com/doganarif/giq/releases/latest/download/giq_linux_arm64.tar.gz'
     end
   end
 
-  depends_on "git"
+  depends_on 'git'
 
   def install
-    bin.install "giq"
+    bin.install 'giq'
   end
 
   test do
-    system "#{bin}/giq", "--version"
+    system "#{bin}/giq", '--version'
   end
 end
