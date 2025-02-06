e7abd37e15875ccb72302a21a078e12fbca8b6ab
Arif Dogan <me@arif.sh>
2025-02-06 16:31:58 +0100

    Simplify giq.rb by updating URL handling and adding livecheck for versioning.

--- formula/giq.rb
+++ formula/giq.rb
@@ -1,44 +1,33 @@
 class Giq < Formula
   desc 'Git wrapper enhanced with AI-powered features'
   homepage 'https://github.com/doganarif/giq'
-  url 'https://api.github.com/repos/doganarif/giq/releases/latest', using: :nounzip
-  license 'MIT'
+  version '0.1.3'
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url do
-        require 'json'
-        json = JSON.parse(File.read(@resource.cached_download))
-        latest_release = json['tag_name']
-        "https://github.com/doganarif/giq/releases/download/#{latest_release}/giq_#{latest_release.delete_prefix('v')}_darwin_arm64.tar.gz"
+  stable do
+    on_macos do
+      if Hardware::CPU.arm?
+        url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_darwin_arm64.tar.gz"
+      else
+        url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_darwin_amd64.tar.gz"
       end
-    else
-      url do
-        require 'json'
-        json = JSON.parse(File.read(@resource.cached_download))
-        latest_release = json['tag_name']
-        "https://github.com/doganarif/giq/releases/download/#{latest_release}/giq_#{latest_release.delete_prefix('v')}_darwin_amd64.tar.gz"
+    end
+
+    on_linux do
+      if Hardware::CPU.intel?
+        url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_linux_amd64.tar.gz"
+      elsif Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+        url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_linux_arm64.tar.gz"
       end
     end
   end
 
-  on_linux do
-    if Hardware::CPU.intel?
-      url do
-        require 'json'
-        json = JSON.parse(File.read(@resource.cached_download))
-        latest_release = json['tag_name']
-        "https://github.com/doganarif/giq/releases/download/#{latest_release}/giq_#{latest_release.delete_prefix('v')}_linux_amd64.tar.gz"
-      end
-    end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url do
-        require 'json'
-        json = JSON.parse(File.read(@resource.cached_download))
-        latest_release = json['tag_name']
-        "https://github.com/doganarif/giq/releases/download/#{latest_release}/giq_#{latest_release.delete_prefix('v')}_linux_arm64.tar.gz"
-      end
-    end
+  head do
+    url 'https://github.com/doganarif/giq.git', branch: 'main'
+  end
+
+  livecheck do
+    url :stable
+    strategy :github_latest
   end
 
   depends_on 'git'
