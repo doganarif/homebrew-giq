a074c7f5ec7f7a22b8b17b7ce2512cd6b3d9d79e
Arif Dogan <me@arif.sh>
2025-02-06 16:30:35 +0100

    Update giq.rb to dynamically fetch the latest release URL using JSON parsing.

--- formula/giq.rb
+++ formula/giq.rb
@@ -1,27 +1,43 @@
 class Giq < Formula
   desc 'Git wrapper enhanced with AI-powered features'
   homepage 'https://github.com/doganarif/giq'
-  version '0.1.2'
+  url 'https://api.github.com/repos/doganarif/giq/releases/latest', using: :nounzip
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_darwin_arm64.tar.gz"
-      sha256 'LEAVE_BLANK' # GoReleaser will update this
+      url do
+        require 'json'
+        json = JSON.parse(File.read(@resource.cached_download))
+        latest_release = json['tag_name']
+        "https://github.com/doganarif/giq/releases/download/#{latest_release}/giq_#{latest_release.delete_prefix('v')}_darwin_arm64.tar.gz"
+      end
     else
-      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_darwin_amd64.tar.gz"
-      sha256 'LEAVE_BLANK' # GoReleaser will update this
+      url do
+        require 'json'
+        json = JSON.parse(File.read(@resource.cached_download))
+        latest_release = json['tag_name']
+        "https://github.com/doganarif/giq/releases/download/#{latest_release}/giq_#{latest_release.delete_prefix('v')}_darwin_amd64.tar.gz"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_linux_amd64.tar.gz"
-      sha256 'LEAVE_BLANK' # GoReleaser will update this
+      url do
+        require 'json'
+        json = JSON.parse(File.read(@resource.cached_download))
+        latest_release = json['tag_name']
+        "https://github.com/doganarif/giq/releases/download/#{latest_release}/giq_#{latest_release.delete_prefix('v')}_linux_amd64.tar.gz"
+      end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_linux_arm64.tar.gz"
-      sha256 'LEAVE_BLANK' # GoReleaser will update this
+      url do
+        require 'json'
+        json = JSON.parse(File.read(@resource.cached_download))
+        latest_release = json['tag_name']
+        "https://github.com/doganarif/giq/releases/download/#{latest_release}/giq_#{latest_release.delete_prefix('v')}_linux_arm64.tar.gz"
+      end
     end
   end
 
