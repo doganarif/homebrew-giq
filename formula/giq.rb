3efff3ed4c147e6bba892b86c3a2321e0320306f
Arif Dogan <me@arif.sh>
2025-02-06 16:29:10 +0100

    Update giq.rb to version 0.1.2 with dynamic URL and placeholder SHA256 for releases.

--- formula/giq.rb
+++ formula/giq.rb
@@ -1,26 +1,27 @@
 class Giq < Formula
   desc 'Git wrapper enhanced with AI-powered features'
   homepage 'https://github.com/doganarif/giq'
+  version '0.1.2'
   license 'MIT'
-  head 'https://github.com/doganarif/giq.git', branch: 'main'
-
-  livecheck do
-    url :stable
-    strategy :github_latest
-  end
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/doganarif/giq/releases/latest/download/giq_darwin_arm64.tar.gz'
+      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_darwin_arm64.tar.gz"
+      sha256 'LEAVE_BLANK' # GoReleaser will update this
     else
-      url 'https://github.com/doganarif/giq/releases/latest/download/giq_darwin_amd64.tar.gz'
+      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_darwin_amd64.tar.gz"
+      sha256 'LEAVE_BLANK' # GoReleaser will update this
     end
   end
 
   on_linux do
-    url 'https://github.com/doganarif/giq/releases/latest/download/giq_linux_amd64.tar.gz' if Hardware::CPU.intel?
+    if Hardware::CPU.intel?
+      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_linux_amd64.tar.gz"
+      sha256 'LEAVE_BLANK' # GoReleaser will update this
+    end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/doganarif/giq/releases/latest/download/giq_linux_arm64.tar.gz'
+      url "https://github.com/doganarif/giq/releases/download/v#{version}/giq_#{version}_linux_arm64.tar.gz"
+      sha256 'LEAVE_BLANK' # GoReleaser will update this
     end
   end
 
